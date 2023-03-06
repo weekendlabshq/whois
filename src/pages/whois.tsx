4181afbd103d7cd0c35230dfa0e1367588b2f881
Owen Bick <owenbick@gmail.com>
2023-03-05 22:41:32 -0500

    Sitemap

---
+++ src/pages/whois.tsx
@@ -0,0 +1,75 @@
+import React, { useState, FormEvent } from 'react';
+import Head from 'next/head';
+import Link from 'next/link';
+import Layout from '../../components/Layout';
+import Header from '../../components/Header';
+import Footer from '../../components/Footer';
+import { useRouter } from 'next/router';
+import { IoSearchSharp } from "react-icons/io5";
+import { GoogleAdSense } from "nextjs-google-adsense";
+
+export default function Home(): JSX.Element {
+  const router = useRouter();
+  const [domain, setDomain] = useState<string>('');
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const domainRegex = /^[a-zA-Z0-9][a-zA-Z0-9-]{0,60}[a-zA-Z0-9]?\.[a-zA-Z]{2,}$/;
+
+    if (domain.trim() !== '') {
+      if (!domainRegex.test(domain)) {
+        alert('🫤 Sorry, that is not a valid domain name.');
+        return;
+      }
+      router.push(`/whois/${domain}`);
+    }
+  };
+
+  return (
+    <>
+      <Head>
+        <title>WHOIS by Weekend Labs</title>
+        <link rel="icon" href="./static/favicon.ico" />
+        <meta
+          name="description"
+          content="Weekend Labs is a full-service software consultancy based in Boston."
+        />
+        <meta name="author" content="Weekend Labs" />
+      </Head>
+      <Header />
+      <Layout>
+        <section className="hero">
+          <div className="container">
+            <h1>WHOIS by <br className='no-desktop' /> Weekend Labs</h1>
+            <p>Unlock the secrets of any domain with just one WHOIS search.</p>
+            <form onSubmit={handleSubmit}>
+              <input
+                type="text"
+                value={domain}
+                onChange={(e) => setDomain(e.target.value)}
+                placeholder="Type in any domain name..."
+              />
+              <button type="submit"><IoSearchSharp />&nbsp;Search&nbsp;<span>WHOIS</span></button>
+            </form>
+          </div>
+        </section>
+        <section className='ad'>
+          <div className='container'>
+            <div className='ad__frame'>
+              <GoogleAdSense publisherId="pub-4778248135015291" />
+            </div>
+          </div>
+        </section>
+        <section className='about'>
+          <div className='container'>
+            <h1>What is WHOIS?</h1>
+            <p>WHOIS is a protocol that provides information about domain names, IP addresses, and other resources registered with the Internet Assigned Numbers Authority (IANA). It is a public database that stores information about domain name registrants, such as their name, address, email, phone number, and other relevant information.</p>
+            <p>The WHOIS protocol is used to query this database and retrieve information about a domain name, including its registration and expiration date, registrar information, and name servers. This information is helpful for website owners, domain buyers and sellers, and law enforcement agencies.</p>
+            <p>While the WHOIS database is publicly accessible, some registrars and domain privacy services may provide a way to hide the registrant&apos;s personal information from public view. In general, domain registrars are required to collect and maintain accurate and up-to-date WHOIS information for their customers.</p>
+          </div>
+        </section>
+      </Layout>
+      <Footer />
+    </>
+  );
+}
